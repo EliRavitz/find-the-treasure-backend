306f2b58201ca783e1828dc07d7397f48515f454
Eli Ravitz <eliravi1232@gmail.com>
2023-02-18 21:16:11 +0200

    03

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(
     credentials: true,
   })
 )
-
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', req.headers.origin)
   res.header('Access-Control-Allow-Credentials', true)
@@ -32,6 +31,7 @@ app.use(function (req, res, next) {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   )
+
   next()
 })
 
