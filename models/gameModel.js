a6a580d74cebe095298744d5ffcd53818344300d
Eli Ravitz <eliravi1232@gmail.com>
2023-02-19 15:12:27 +0200

    23

--- models/gameModel.js
+++ models/gameModel.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose')
 const gameSchema = new mongoose.Schema({
   name: {
     type: String,
-
     required: [true, 'A game must have a name'],
     trim: true,
     maxlength: [20, 'A gmae name must have less or equal then 20 characters'],
     minlength: [3, 'A gmae name must have more or equal then 3 characters'],
+    unique: false,
   },
   stations: Array,
   gameInProgress: {
