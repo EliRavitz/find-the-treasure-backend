70dd982eaedf66df3da90b16f5cf931d80d74eb0
Eli Ravitz <eliravi1232@gmail.com>
2023-02-19 11:59:07 +0200

    09

--- server.js
+++ server.js
@@ -13,31 +13,21 @@ const app = require('./app')
 
 const DB = process.env.DATABASE
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(DB, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    })
-  } catch (error) {
-    console.log(error)
-    process.exit(1)
-  }
-}
-const port = process.env.PORT || 5000
-
-let server
-connectDB()
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('DB connection successful! 😀')
   })
-  .then(() => {
-    server = app.listen(port, () => {
-      console.log(`App running on port ${port}...`)
-    })
-  })
+
+const port = process.env.PORT || 5000
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`)
+})
 
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...')
@@ -46,3 +36,52 @@ process.on('unhandledRejection', (err) => {
     process.exit(1)
   })
 })
+
+// const mongoose = require('mongoose')
+// const dotenv = require('dotenv')
+
+// process.on('uncaughtException', (err) => {
+//   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...')
+//   console.log(err.name, err.message)
+//   console.log(err)
+//   process.exit(1)
+// })
+
+// dotenv.config({ path: './config.env' })
+// const app = require('./app')
+
+// const DB = process.env.DATABASE
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(DB, {
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//       useUnifiedTopology: true,
+//     })
+//   } catch (error) {
+//     console.log(error)
+//     process.exit(1)
+//   }
+// }
+// const port = process.env.PORT || 5000
+
+// let server
+// connectDB()
+//   .then(() => {
+//     console.log('DB connection successful! 😀')
+//   })
+//   .then(() => {
+//     server = app.listen(port, () => {
+//       console.log(`App running on port ${port}...`)
+//     })
+//   })
+
+// process.on('unhandledRejection', (err) => {
+//   console.log('UNHANDLED REJECTION! 💥 Shutting down...')
+//   console.log(err.name, err.message, err)
+//   server.close(() => {
+//     process.exit(1)
+//   })
+// })
